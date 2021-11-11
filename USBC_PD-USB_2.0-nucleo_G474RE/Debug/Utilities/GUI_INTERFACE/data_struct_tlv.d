f5b22cc23d45c6b26fef35ce2ca59927cc35b884
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2021-11-11 18:36:00 +0100

    Remove build from branch

--- USBC_PD-USB_2.0-nucleo_G474RE/Debug/Utilities/GUI_INTERFACE/data_struct_tlv.d
+++
@@ -1,5 +0,0 @@
-Utilities/GUI_INTERFACE/data_struct_tlv.o: \
- ../Utilities/GUI_INTERFACE/data_struct_tlv.c \
- ../Utilities/GUI_INTERFACE/data_struct_tlv.h
-
-../Utilities/GUI_INTERFACE/data_struct_tlv.h:
--- USBC_PD-USB_2.0-nucleo_G474RE/Debug/objects.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l:USBPDCORE_PD3_FULL_CM4_wc32.a
-
